15ea5d167fec78744b3d084665cce6e81b8b7e3d
kantoralex <kantor_alex@msn.com>
2014-07-21 07:17:43 -0700

    Fixing build issues after testing on a clean PC

--- InstallToolchain.sh
+++ InstallToolchain.sh
@@ -3,6 +3,6 @@ sudo add-apt-repository ppa:terry.guo/gcc-arm-embedded
 sudo apt-get update
 sudo apt-get install scons gcc-arm-none-eabi
 sudo apt-get install exuberant-ctags fabric
-
+tar zxf nanopb-0.2.8-linux-x86.tar.gz -C ~ 
 
 
--- RebuildAll.sh
+++ RebuildAll.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-sudo chmod +X setup.sh
-./setup.sh
+
+export NANOPB_PATH=~/nanopb-0.2.8-linux-x86/generator
+export PATH=$PATH:$NANOPB_PATH
 
 # clean build for the opencm3 library
 cd libopencm3 
 make clean
 make
+cd -
 
 # build the nanopb
 cd interface
