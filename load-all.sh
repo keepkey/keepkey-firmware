ae3068578ea04a6c81706dc9e61d86a2edd60089
Darin Stanchfield <darin@keepkey.com>
2014-10-06 10:11:09 -0700

    renamed

--- load-all.sh
+++ load-all.sh
@@ -7,7 +7,7 @@ BINDIR=./build/arm-none-gnu-eabi/debug/bin/
 BINNAME=bootloader_main
 ELF_FILE=$BINDIR/$BINNAME.elf
 
-openocd -s /usr/share/openocd/scripts -f /usr/share/openocd/scripts/interface/jlink.cfg -f /usr/share/openocd/scripts/board/keepkey_board.cfg -c "program $ELF_FILE verify"
+openocd -s /usr/share/openocd/scripts -f interface/jlink.cfg -f board/keepkey_board.cfg -c "program $ELF_FILE verify"
 if [[ $? -ne 0 ]]; then
 	echo $?
         echo "error in bootloading"
@@ -18,10 +18,9 @@ INDIR=./build/arm-none-gnu-eabi/debug/bin/
 BINAPPNAME=keepkey_main
 ELF_FILE=$BINDIR/$BINAPPNAME.elf
 
-openocd -s /usr/share/openocd/scripts -f interface/jlink.cfg -f /usr/share/openocd/scripts/board/keepkey_board.cfg -c "program $ELF_FILE verify"
+openocd -s /usr/share/openocd/scripts -f interface/jlink.cfg -f board/keepkey_board.cfg -c "program $ELF_FILE verify"
 if [[ $? -ne 0 ]]; then
 	echo $?
         echo "error in loading application image"
 	exit
 fi
-
