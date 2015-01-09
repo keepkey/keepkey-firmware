a57e4e69249ce593f37a2adca11e829af538016f
Philip Khoo <philip@keepkey.com>
2015-01-08 17:25:34 -0800

    Added license header to usb.driver.c

--- keepkey_board/local/baremetal/usb_driver.c
+++ keepkey_board/local/baremetal/usb_driver.c
@@ -1,3 +1,25 @@
+/* START KEEPKEY LICENSE */
+/*
+ * This file is part of the KeepKey project.
+ *
+ * Copyright (C) 2014 KeepKey LLC
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/* END KEEPKEY LICENSE */
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
