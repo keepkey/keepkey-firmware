63b92679fc5f4353bba3df299415c452ade43a12
Philip Khoo <philip@keepkey.com>
2015-01-12 17:23:54 -0800

    coding standard updates for pin.c

--- keepkey_board/local/baremetal/pin.c
+++ keepkey_board/local/baremetal/pin.c
@@ -1,12 +1,24 @@
-/******************************************************************************
-    Copyright (c) __20xx __Client_Name. All rights reserved.
-    Developed for __Client_Name by Carbon Design Group.
-******************************************************************************/
-
-/// @file pin.c
-/// Basic pin functions.
-///
-
+/* START KEEPKEY LICENSE */
+/*
+ * This file is part of the KeepKey project.
+ *
+ * Copyright (C) 2014 KeepKey LLC
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/* END KEEPKEY LICENSE */
 
 //================================ INCLUDES ===================================
 
@@ -15,50 +27,34 @@
 #include <libopencm3/stm32/gpio.h>
 #include "pin.h"
 
-
-//====================== CONSTANTS, TYPES, AND MACROS =========================
-
-
-//=============================== VARIABLES ===================================
-
-
-//====================== PRIVATE FUNCTION DECLARATIONS ========================
-
-
-//=============================== FUNCTIONS ===================================
-
-
-//-----------------------------------------------------------------------------
-// See pin.h for public interface.
-//
-void
-pin_init_output(
-        const Pin*  pin,
-        OutputMode  output_mode,
-        PullMode    pull_mode
-)
+/*
+ * pin_init_output() - initialize GPIO for led
+ *
+ * INPUT - 
+ *      *pin - pointer pin assignment 
+ *      output_mode - pin output state 
+ *      pull_mode - input mode 
+ * OUTPUT - 
+ */
+void pin_init_output(const Pin *pin, OutputMode output_mode, PullMode pull_mode)
 {
     uint8_t output_mode_setpoint;
     uint8_t pull_mode_setpoint;
 
-    switch( output_mode )
-    {
+    switch( output_mode ) {
         case OPEN_DRAIN_MODE:
             output_mode_setpoint = GPIO_OTYPE_OD;
             break;
-
         case PUSH_PULL_MODE:
         default:
             output_mode_setpoint = GPIO_OTYPE_PP;
             break;
     }
 
-    switch( pull_mode )
-    {
+    switch( pull_mode ) {
         case PULL_UP_MODE:
             pull_mode_setpoint = GPIO_PUPD_PULLUP;
             break;
-
         case PULL_DOWN_MODE:
             pull_mode_setpoint = GPIO_PUPD_PULLDOWN;
             break;
@@ -70,15 +66,6 @@ pin_init_output(
     }
 
     // Set up port A
-    gpio_mode_setup( 
-            pin->port, 
-            GPIO_MODE_OUTPUT, 
-            pull_mode_setpoint, 
-            pin->pin );
-
-    gpio_set_output_options(
-            pin->port,
-            output_mode_setpoint,
-            GPIO_OSPEED_100MHZ,
-            pin->pin );
+    gpio_mode_setup( pin->port, GPIO_MODE_OUTPUT, pull_mode_setpoint, pin->pin );
+    gpio_set_output_options( pin->port, output_mode_setpoint, GPIO_OSPEED_100MHZ, pin->pin );
 }
