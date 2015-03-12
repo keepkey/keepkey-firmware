c4c972c48bd0390e367207ff5007228360344e31
Philip Khoo <philip@keepkey.com>
2015-03-12 11:34:17 -0700

    Updated jlink loa^Cr shell script to include bootstrap loading in Jenkins

--- load-all.sh
+++ load-all.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
 
 BINDIR=./build/arm-none-gnu-eabi/debug/bin/
+BSTRAPNAME=bootstrap_main
+ELF_FILE=$BINDIR/$BSTRAPNAME.elf
+
+openocd -s /usr/share/openocd/scripts -f interface/jlink.cfg -f board/keepkey_board.cfg -c "program $ELF_FILE verify"
+if [[ $? -ne 0 ]]; then
+        echo $?
+        echo "error in loading bootstrap"
+        exit
+fi
+
+BINDIR=./build/arm-none-gnu-eabi/debug/bin/
 BINNAME=bootloader_main
 ELF_FILE=$BINDIR/$BINNAME.elf
 
 openocd -s /usr/share/openocd/scripts -f interface/jlink.cfg -f board/keepkey_board.cfg -c "program $ELF_FILE verify"
 if [[ $? -ne 0 ]]; then
-	echo $?
-        echo "error in bootloading"
-	exit
+        echo $?
+        echo "error in loading bootloader"
+        exit
 fi
 
 INDIR=./build/arm-none-gnu-eabi/debug/bin/
@@ -17,7 +28,7 @@ ELF_FILE=$BINDIR/$BINAPPNAME.elf
 
 openocd -s /usr/share/openocd/scripts -f interface/jlink.cfg -f board/keepkey_board.cfg -c "program $ELF_FILE verify reset"
 if [[ $? -ne 0 ]]; then
-	echo $?
+        echo $?
         echo "error in loading application image"
-	exit
+        exit
 fi
