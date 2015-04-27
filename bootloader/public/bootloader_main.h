82912657813b7fed68f9f23cf204ae6911aba169
Darin Stanchfield <darin@keepkey.com>
2015-04-27 14:13:05 -0700

    moved out signatures to own file

---
+++ bootloader/public/bootloader_main.h
@@ -0,0 +1,37 @@
+/* START KEEPKEY LICENSE */
+/*
+ * This file is part of the KeepKey project.
+ *
+ * Copyright (C) 2015 KeepKey LLC
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* END KEEPKEY LICENSE */
+
+/* prevent duplicate inclusion */
+#ifndef BOOTLOADER_MAIN_H
+#define BOOTLOADER_MAIN_H
+
+/*********************  #defines *************************/
+
+/***************** typedefs and enums  *******************/
+typedef void (*app_entry_t)(void);
+
+
+/***************** Function Declaration ******************/
+bool check_fw_is_new(void);
+
+#endif
