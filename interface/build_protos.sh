bdd3c7404804ea90ca6a6f896ef75889135f93f6
Darin Stanchfield <darin@keepkey.com>
2015-05-11 11:28:07 -0700

    no longer used

--- interface/build_protos.sh
+++
@@ -1,8 +0,0 @@
-pushd public
-for i in $(ls *.proto)
-do
-    protoc -I. -I/usr/include --plugin=nanopb=/home/tom/src/nanopb/generator/protoc-gen-nanopb --nanopb_out=. $i
-done
-mv *.pb.c ../local
-popd
-
