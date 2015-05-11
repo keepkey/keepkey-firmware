472e55ed7bf5972e996b313de63e83508fe38c87
Darin Stanchfield <darin@keepkey.com>
2015-05-11 13:17:39 -0700

    renamed

--- docker_build_protos.sh
+++ docker_build_protos.sh
@@ -7,11 +7,11 @@ git clone git@gitlab.keepkey.com:client-software/device-protocol.git $TEMPDIR
 
 docker build -t $IMAGETAG .
 
-docker run -t -v $(pwd):/keepkey-firmware -v $(pwd)/$TEMPDIR:/root/$TEMPDIR --rm $IMAGETAG /bin/sh -c "\
+docker run -t -v $(pwd):/root/keepkey-firmware -v $(pwd)/$TEMPDIR:/root/$TEMPDIR --rm $IMAGETAG /bin/sh -c "\
 	cd /root/$TEMPDIR && \
-	cp /keepkey-firmware/interface/public/*.options . && \
+	cp /root/keepkey-firmware/interface/public/*.options . && \
 	protoc -I. -I/usr/include --plugin=nanopb=protoc-gen-nanopb --nanopb_out=. *.proto && \
-	mv *.pb.c /keepkey-firmware/interface/local && \
-	mv *.pb.h /keepkey-firmware/interface/public"
+	mv *.pb.c /root/keepkey-firmware/interface/local && \
+	mv *.pb.h /root/keepkey-firmware/interface/public"
 
 rm -rf $TEMPDIR
