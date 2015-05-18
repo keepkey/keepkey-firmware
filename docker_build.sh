c978339d4f175a2151d5f69b38d3ab60b26b5bc0
Darin Stanchfield <darin@keepkey.com>
2015-05-18 14:52:23 -0700

    added ability to docker build

---
+++ docker_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGETAG=keepkey/firmware
+
+docker build -t $IMAGETAG .
+
+docker run -t -v $(pwd):/root/keepkey-firmware --rm $IMAGETAG /bin/sh -c "\
+	cd /root/keepkey-firmware/libopencm3 && \
+	make clean && \
+  make && \
+	cd /root/keepkey-firmware && \
+	./b"
