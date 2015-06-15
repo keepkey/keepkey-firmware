2a376a5036036607144dc997a0d75714ab1676e5
Darin Stanchfield <darin@Darins-iMac.local>
2015-06-15 16:51:22 -0700

    added script for firmware release building

---
+++ docker_build_release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "*********************************************************************"
+echo "* You are about to build a release version of KeepKey firmware. The *"
+echo "* resulting bootloader image will memory protect the flash on your  *"
+echo "* device, so please use it with extreme care.                       *"
+echo "*********************************************************************"
+read -p "Are you sure you want to continue? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+IMAGETAG=keepkey/firmware
+
+docker build -t $IMAGETAG .
+
+docker run -t -v $(pwd):/root/keepkey-firmware --rm $IMAGETAG /bin/sh -c "\
+  rm -rf build && \
+	cd /root/keepkey-firmware/libopencm3 && \
+	make clean && \
+  make && \
+	cd /root/keepkey-firmware && \
+	./b -mp"
+
+fi
