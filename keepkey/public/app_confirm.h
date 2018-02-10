3adcaf024b0c9542ff962c003bfd7eb617b63a09
Scott <scott@keepkey.com>
2018-02-10 15:19:50 -0700

    rm sign identity functions
    as they are not currently supported and are extraneous

--- keepkey/public/app_confirm.h
+++ keepkey/public/app_confirm.h
@@ -46,6 +46,5 @@ bool confirm_transaction(const char *total_amount, const char *fee);
 bool confirm_load_device(bool is_node);
 bool confirm_address(const char *desc, const char *address);
 bool confirm_xpub(const char *xpub);
-bool confirm_sign_identity(const IdentityType *identity, const char *challenge);
 bool confirm_ethereum_address(const char *desc, const char *address);
 #endif
