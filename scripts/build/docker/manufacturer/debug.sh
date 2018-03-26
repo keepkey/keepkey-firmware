608550a76cab9861b228f932b17486fd7476c795
keepkeyjon <jon@keepkey.com>
2018-03-26 17:10:19 -0600

    Add build scripts for manufacturing firmware

---
+++ scripts/build/docker/manufacturer/debug.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IMAGETAG=kktech/firmware:v5-beta
+
+docker pull $IMAGETAG
+
+docker run -t \
+  -v $(pwd):/root/keepkey-firmware \
+  -v $(pwd)/deps/device-protocol:/root/keepkey-firmware/deps/device-protocol $IMAGETAG /bin/sh -c "\
+      mkdir /root/build && cd /root/build && \
+      cmake -C /root/keepkey-firmware/cmake/caches/manufacturer.cmake /root/keepkey-firmware \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DKK_DEBUG_LINK=ON && \
+      make && \
+      mkdir -p /root/keepkey-firmware/bin && \
+      cp bin/*.bin /root/keepkey-firmware/bin/ && \
+      cp bin/*.elf /root/keepkey-firmware/bin/"
