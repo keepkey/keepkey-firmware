8b961eb05a12170cab6458991bd754b78c568225
Scott <scott@keepkey.com>
2018-04-03 15:49:00 -0600

    rm color from jenkins builds

--- scripts/build/docker/device/debug.sh
+++ scripts/build/docker/device/debug.sh
@@ -7,13 +7,16 @@ IMAGETAG=kktech/firmware:v6-beta
 
 docker pull $IMAGETAG
 
+if [ "$(whoami)" == "root" ]; then COLOR='OFF'; else COLOR='ON'; fi
+
 docker run -t \
   -v $(pwd):/root/keepkey-firmware \
   $IMAGETAG /bin/sh -c "\
       mkdir /root/build && cd /root/build && \
       cmake -C /root/keepkey-firmware/cmake/caches/device.cmake /root/keepkey-firmware \
         -DCMAKE_BUILD_TYPE=Debug \
-        -DKK_DEBUG_LINK=ON && \
+        -DKK_DEBUG_LINK=ON \
+        -DCMAKE_COLOR_MAKEFILE=$COLOR &&\
       make && \
       mkdir -p /root/keepkey-firmware/bin && \
       cp bin/*.bin /root/keepkey-firmware/bin/ && \
--- scripts/build/docker/device/release.sh
+++ scripts/build/docker/device/release.sh
@@ -7,12 +7,18 @@ IMAGETAG=kktech/firmware:v6-beta
 
 docker pull $IMAGETAG
 
+if [ "$(whoami)" == "root" ]; then COLOR='OFF'; else COLOR='ON'; fi
+ls -alh deps
+ls -alh deps/device-protocol
+
 docker run -t \
   -v $(pwd):/root/keepkey-firmware \
   $IMAGETAG /bin/sh -c "\
       mkdir /root/build && cd /root/build && \
       cmake -C /root/keepkey-firmware/cmake/caches/device.cmake /root/keepkey-firmware \
-        -DCMAKE_BUILD_TYPE=Release && \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_COLOR_MAKEFILE=$COLOR &&\
       make && \
       mkdir -p /root/keepkey-firmware/bin && \
-      cp bin/*.bin /root/keepkey-firmware/bin/"
+      cp bin/*.bin /root/keepkey-firmware/bin/ && \
+      cp bin/*.elf /root/keepkey-firmware/bin/"
