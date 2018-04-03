9906de17dd0112456c1fc6cb1e36ee5180e4e2a0
Scott <scott@keepkey.com>
2018-04-03 15:34:38 -0600

    update device protocol

--- scripts/build/docker/device/debug.sh
+++ scripts/build/docker/device/debug.sh
@@ -9,7 +9,7 @@ docker pull $IMAGETAG
 
 docker run -t \
   -v $(pwd):/root/keepkey-firmware \
-  -v $(pwd)/deps/device-protocol:/root/keepkey-firmware/deps/device-protocol $IMAGETAG /bin/sh -c "\
+  $IMAGETAG /bin/sh -c "\
       mkdir /root/build && cd /root/build && \
       cmake -C /root/keepkey-firmware/cmake/caches/device.cmake /root/keepkey-firmware \
         -DCMAKE_BUILD_TYPE=Debug \
