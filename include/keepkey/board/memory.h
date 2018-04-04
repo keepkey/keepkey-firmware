e5d1ba09ab69275488ee3f9f67af305a91f29aa6
keepkeyjon <jon@keepkey.com>
2018-04-04 07:43:27 -0600

    We rotate through sectors 1-3 for wear leveling. NFC

--- include/keepkey/board/memory.h
+++ include/keepkey/board/memory.h
@@ -33,9 +33,9 @@
    name    |          range          |  size   |     function
 -----------+-------------------------+---------+------------------
  Sector  0 | 0x08000000 - 0x08003FFF |  16 KiB | bootstrap code (Read Only)
- Sector  1 | 0x08004000 - 0x08007FFF |  16 KiB | empty(Read/Write)
+ Sector  1 | 0x08004000 - 0x08007FFF |  16 KiB | storage/config (Read/Write)
 -----------+-------------------------+---------+------------------
- Sector  2 | 0x08008000 - 0x0800BFFF |  16 KiB | empty (Read/Write)
+ Sector  2 | 0x08008000 - 0x0800BFFF |  16 KiB | storage/config (Read/Write)
  Sector  3 | 0x0800C000 - 0x0800FFFF |  16 KiB | storage/config (Read/Write)
 -----------+-------------------------+---------+------------------
  Sector  4 | 0x08010000 - 0x0801FFFF |  64 KiB | empty (Read/Write)
