keepkey/keepkey-firmware

commit f877d613e24a7b8e11d8d46194e990641b0432e7

Constant time recovery entry. v2

lib/firmware/recovery.c
@@ -133,55 +133,58 @@ void recovery_init(uint32_t _word_count, bool passphrase_protection, bool pin_pr
 	next_word();
 }
 
+static bool isInWordList(const char *word) {
+    const char * const *wl = mnemonic_wordlist();
+    while (*wl)
+    {
+        if (strcmp(word, *wl) == 0)
+        {
+            // Yes, this "leaks" timing information about what the word
+            // is, but in this recovery mode, the host knows what each
+            // of the words are (just not their order, nor which ones
+            // are fake).
+            return true;
+        }
+        wl++;
+    }
+    return false;
+}
+
 void recovery_word(const char *word)
 {
-    if (!awaiting_word) 
+    if (!awaiting_word)
     {
         fsm_sendFailure(FailureType_Failure_UnexpectedMessage, "Not in Recovery mode");
         go_home();
         return;
     }
 
-    if (word_pos == 0) 
-    { // fake word
-        if (strcmp(word, fake_word) != 0) {
+    volatile bool found = isInWordList(word);
+    volatile bool isFake = exact_str_match(word, fake_word, strlen(word) + 1) != 0;
+
+    if ((word_pos == 0) & isFake) {
+        // Fake word
+        storage_reset();
+        fsm_sendFailure(FailureType_Failure_SyntaxError, "Wrong word retyped");
+        go_home();
+        return;
+    } else {
+        // Real word
+        if (enforce_wordlist & (!found)) {
             storage_reset();
-            fsm_sendFailure(FailureType_Failure_SyntaxError, "Wrong word retyped");
+            fsm_sendFailure(FailureType_Failure_SyntaxError, "Word not found in the bip39 wordlist");
             go_home();
             return;
-        }
-    } else { // real word
-        if (enforce_wordlist) 
-        { // check if word is valid
-            const char * const *wl = mnemonic_wordlist();
-            bool found = false;
-            while (*wl) 
-            {
-                if (strcmp(word, *wl) == 0) 
-                {
-                    found = true;
-                    break;
-                }
-                wl++;
-            }
-            if (!found) 
-            {
-                storage_reset();
-                fsm_sendFailure(FailureType_Failure_SyntaxError, "Word not found in a wordlist");
-                go_home();
-                return;
-            }
         }
         strlcpy(words[word_pos - 1], word, sizeof(words[word_pos - 1]));
     }
 
-    if (word_index + 1 == 24)
-    { // last one
+    if (word_index + 1 == 24) {
+        // last one
         storage_set_mnemonic_from_words((const char (*)[])words, word_count);
 
-        if (!enforce_wordlist || mnemonic_check(storage_get_shadow_mnemonic()))
-        {
-        	storage_commit();
+        if (!enforce_wordlist || mnemonic_check(storage_get_shadow_mnemonic())) {
+            storage_commit();
             fsm_sendSuccess("Device recovered");
         } else {
             storage_reset();

commit dffb8ded68faf4184c3acd41cbd028a3d774d90f

Fix a bug introduced in f877d613
... which caused fake words to not be recognized.

lib/firmware/recovery.c
@@ -160,14 +160,16 @@ void recovery_word(const char *word)
     }
 
     volatile bool found = isInWordList(word);
-    volatile bool isFake = exact_str_match(word, fake_word, strlen(word) + 1) != 0;
+    volatile bool isCorrectFake = exact_str_match(word, fake_word, strlen(word) + 1);
 
-    if ((word_pos == 0) & isFake) {
-        // Fake word
-        storage_reset();
-        fsm_sendFailure(FailureType_Failure_SyntaxError, "Wrong word retyped");
-        go_home();
-        return;
+    if (word_pos == 0) {
+        if (!isCorrectFake) {
+            // Fake word
+            storage_reset();
+            fsm_sendFailure(FailureType_Failure_SyntaxError, "Wrong word retyped");
+            go_home();
+            return;
+        }
     } else {
         // Real word
         if (enforce_wordlist & (!found)) {

lib/firmware/recovery_cipher.c
@@ -121,7 +121,7 @@ static void get_current_word(char *current_word)
 
 bool exact_str_match(const char *str1, const char *str2, uint32_t len)
 {
-    uint32_t match = 0;
+    volatile uint32_t match = 0;
 
     // Access through volatile ptrs to prevent compiler optimizations that
     // might leak timing information.
@@ -133,9 +133,7 @@ bool exact_str_match(const char *str1, const char *str2, uint32_t len)
         if(str1_v[i] == str2_v[i])
         {
             match++;
-        }
-        else
-        {
+        } else {
             match--;
         }
     }