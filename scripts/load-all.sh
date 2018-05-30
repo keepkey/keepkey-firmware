dc21a1416ff5c328fbd0ab158380a897cc158490
keepkeyjon <jon@keepkey.com>
2018-05-30 16:40:42 -0600

    Upload the whitelabeling too in load-all.sh

--- scripts/load-all.sh
+++ scripts/load-all.sh
@@ -16,6 +16,14 @@ if [[ $? -ne 0 ]]; then
         exit
 fi
 
+BIN_FILE=./bin/variant.keepkey.bin
+openocd -s /usr/share/openocd/scripts -f interface/jlink.cfg -f ./scripts/openocd/stm32f2x.cfg  -c "program $BIN_FILE 0x08010000 verify exit"
+if [[ $? -ne 0 ]]; then
+        echo $?
+        echo "error in loading variant"
+        exit
+fi
+
 BIN_FILE=./bin/firmware.keepkey.bin
 openocd -s /usr/share/openocd/scripts -f interface/jlink.cfg -f ./scripts/openocd/stm32f2x.cfg -c "program $BIN_FILE 0x08080000 verify reset"
 if [[ $? -ne 0 ]]; then
