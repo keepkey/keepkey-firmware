87c961bd7dca5b05e6e5a45b11ee9d3bd0658cbd
keepkeyjon <jon@keepkey.com>
2018-06-21 12:21:47 -0600

    Bitcoin Gold support

--- lib/firmware/coins.c
+++ lib/firmware/coins.c
@@ -48,15 +48,16 @@ true, {32, {(GAS_LIMIT)}},         /* has_gas_limit, gas_limit*/ \
 
 const CoinTable coins = {
         .table = {
-	    {true, "Bitcoin",     true, "BTC",  true,   0, true,     100000, true,   5, true,  6, true, 10, true,  "\x18" "Bitcoin Signed Message:\n",  true, 0x80000000, false, 0, true,   8, false, {0, {0}},                                          false, {0, {0}}},
-	    {true, "Testnet",     true, "TEST", true, 111, true,   10000000, true, 196, true,  3, true, 40, true,  "\x18" "Bitcoin Signed Message:\n",  true, 0x80000001, false, 0, true,   8, false, {0, {0}},                                          false, {0, {0}}},
-	    {true, "BitcoinCash", true, "BCH",  true,   0, true,     500000, true,   5, false, 0, false, 0, true,  "\x18" "Bitcoin Signed Message:\n",  true, 0x80000091, true,  0, true,   8, false, {0, {0}},                                          false, {0, {0}}},
-	    {true, "Namecoin",    true, "NMC",  true,  52, true,   10000000, true,   5, false, 0, false, 0, true,  "\x19" "Namecoin Signed Message:\n", true, 0x80000007, false, 0, true,   8, false, {0, {0}},                                          false, {0, {0}}},
-	    {true, "Litecoin",    true, "LTC",  true,  48, true,    1000000, true,   5, false, 0, false, 0, true,  "\x19" "Litecoin Signed Message:\n", true, 0x80000002, false, 0, true,   8, false, {0, {0}},                                          false, {0, {0}}},
-	    {true, "Dogecoin",    true, "DOGE", true,  30, true, 1000000000, true,  22, false, 0, false, 0, true,  "\x19" "Dogecoin Signed Message:\n", true, 0x80000003, false, 0, true,   8, false, {0, {0}},                                          false, {0, {0}}},
-	    {true, "Dash",        true, "DASH", true,  76, true,     100000, true,  16, false, 0, false, 0, true,  "\x19" "DarkCoin Signed Message:\n", true, 0x80000005, false, 0, true,   8, false, {0, {0}},                                          false, {0, {0}}},
-	    {true, ETHEREUM,      true, "ETH",  true,  NA, true,     100000, true,  NA, false, 0, false, 0, true,  "\x19" "Ethereum Signed Message:\n", true, 0x8000003c, false, 0, true,  18, false, {0, {0}},                                          false, {0, {0}}},
-	    {true, ETHEREUM_CLS,  true, "ETC",  true,  NA, true,     100000, true,  NA, false, 0, false, 0, true,  "\x19" "Ethereum Signed Message:\n", true, 0x8000003d, false, 0, true,  18, false, {0, {0}},                                          false, {0, {0}}},
+	    {true, "Bitcoin",      true, "BTC",  true,   0, true,     100000, true,   5, true,  6, true, 10, true, "\x18" "Bitcoin Signed Message:\n",      true, 0x80000000, false, 0, true,   8, false, {0, {0}}, false, {0, {0}}},
+	    {true, "Testnet",      true, "TEST", true, 111, true,   10000000, true, 196, true,  3, true, 40, true, "\x18" "Bitcoin Signed Message:\n",      true, 0x80000001, false, 0, true,   8, false, {0, {0}}, false, {0, {0}}},
+	    {true, "BitcoinCash",  true, "BCH",  true,   0, true,     500000, true,   5, false, 0, false, 0, true, "\x18" "Bitcoin Signed Message:\n",      true, 0x80000091, true,  0, true,   8, false, {0, {0}}, false, {0, {0}}},
+	    {true, "Namecoin",     true, "NMC",  true,  52, true,   10000000, true,   5, false, 0, false, 0, true, "\x19" "Namecoin Signed Message:\n",     true, 0x80000007, false, 0, true,   8, false, {0, {0}}, false, {0, {0}}},
+	    {true, "Litecoin",     true, "LTC",  true,  48, true,    1000000, true,   5, false, 0, false, 0, true, "\x19" "Litecoin Signed Message:\n",     true, 0x80000002, false, 0, true,   8, false, {0, {0}}, false, {0, {0}}},
+	    {true, "Dogecoin",     true, "DOGE", true,  30, true, 1000000000, true,  22, false, 0, false, 0, true, "\x19" "Dogecoin Signed Message:\n",     true, 0x80000003, false, 0, true,   8, false, {0, {0}}, false, {0, {0}}},
+	    {true, "Dash",         true, "DASH", true,  76, true,     100000, true,  16, false, 0, false, 0, true, "\x19" "DarkCoin Signed Message:\n",     true, 0x80000005, false, 0, true,   8, false, {0, {0}}, false, {0, {0}}},
+	    {true, ETHEREUM,       true, "ETH",  true,  NA, true,     100000, true,  NA, false, 0, false, 0, true, "\x19" "Ethereum Signed Message:\n",     true, 0x8000003c, false, 0, true,  18, false, {0, {0}}, false, {0, {0}}},
+	    {true, ETHEREUM_CLS,   true, "ETC",  true,  NA, true,     100000, true,  NA, false, 0, false, 0, true, "\x19" "Ethereum Signed Message:\n",     true, 0x8000003d, false, 0, true,  18, false, {0, {0}}, false, {0, {0}}},
+	    {true, "Bitcoin Gold", true, "BTG",  true,  38, true,     500000, true,  23, false, 0, false, 0, true, "\x1d" "Bitcoin Gold Signed Message:\n", true, 0x8000009c, true, 79, true,   8, false, {0, {0}}, false, {0, {0}}},
 	    #include "keepkey/firmware/tokens.def"
         }
 };
