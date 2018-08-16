9be2cc5519db2a4a089be4801aa4b88487cfa252
keepkeyjon <jon@keepkey.com>
2018-08-16 09:05:39 -0600

    Fix terribad whitespace in fsm_msg_ethereum.h. NFC

--- lib/firmware/fsm_msg_ethereum.h
+++ lib/firmware/fsm_msg_ethereum.h
@@ -14,31 +14,31 @@ static int process_ethereum_xfer(const CoinType *coin, EthereumSignTx *msg)
     if(bip44_node_to_string(coin, node_str, msg->to_address_n, msg->to_address_n_count))
     {
         ButtonRequestType button_request = ButtonRequestType_ButtonRequest_ConfirmTransferToAccount;
-	if(is_token_transaction(msg)) {
-	    uint32_t decimal = ethereum_get_decimal(msg->token_shortcut);
-	    if (decimal == 0) {
-		goto process_ethereum_xfer_exit;
-	    }
+        if(is_token_transaction(msg)) {
+            uint32_t decimal = ethereum_get_decimal(msg->token_shortcut);
+            if (decimal == 0) {
+                goto process_ethereum_xfer_exit;
+            }
 
-	    if(ether_token_for_display(msg->token_value.bytes, msg->token_value.size, decimal, token_amount_str, sizeof(token_amount_str)))
-	    {
-		// append token shortcut
-		strncat(token_amount_str, " ", 1);
-		strncat(token_amount_str, msg->token_shortcut, sizeof(msg->token_shortcut));
-		if (!confirm_transfer_output(button_request, token_amount_str, node_str))
-		{
-	   	    ret_val = TXOUT_CANCEL;
-		    goto process_ethereum_xfer_exit;
-		}
-	    }
-	    else 
-	    {
-	   	 goto process_ethereum_xfer_exit;
-	    }
-	}
-	else 
-	{
-       	    if(ether_for_display(msg->value.bytes, msg->value.size, amount_str))
+            if(ether_token_for_display(msg->token_value.bytes, msg->token_value.size, decimal, token_amount_str, sizeof(token_amount_str)))
+            {
+                // append token shortcut
+                strncat(token_amount_str, " ", 1);
+                strncat(token_amount_str, msg->token_shortcut, sizeof(msg->token_shortcut));
+                if (!confirm_transfer_output(button_request, token_amount_str, node_str))
+                {
+                       ret_val = TXOUT_CANCEL;
+                    goto process_ethereum_xfer_exit;
+                }
+            }
+            else
+            {
+                    goto process_ethereum_xfer_exit;
+            }
+        }
+        else
+        {
+                   if(ether_for_display(msg->value.bytes, msg->value.size, amount_str))
             {
                 if(!confirm_transfer_output(button_request, amount_str, node_str))
                 {
@@ -46,36 +46,36 @@ static int process_ethereum_xfer(const CoinType *coin, EthereumSignTx *msg)
                     goto process_ethereum_xfer_exit;
                 }
             }
-	    else 
-	    {
-	   	 goto process_ethereum_xfer_exit;
-	    }
-	}
+            else
+            {
+                    goto process_ethereum_xfer_exit;
+            }
+        }
     }
 
     node = fsm_getDerivedNode(SECP256K1_NAME, msg->to_address_n, msg->to_address_n_count);
-    if(node) 
+    if(node)
     {
-	// setup "token_to" or "to" field depending on if this is a token transaction or not
-	if (is_token_transaction(msg)) {
+        // setup "token_to" or "to" field depending on if this is a token transaction or not
+        if (is_token_transaction(msg)) {
             if(hdnode_get_ethereum_pubkeyhash(node, msg->token_to.bytes))
             {
                 msg->has_token_to = true;
                 msg->token_to.size = 20;
                 ret_val = TXOUT_OK;
-   	    }
-	}
-	else 
-	{
+               }
+        }
+        else
+        {
             if(hdnode_get_ethereum_pubkeyhash(node, msg->to.bytes))
             {
                 msg->has_to = true;
                 msg->to.size = 20;
                 ret_val = TXOUT_OK;
-            } 
-	}
+            }
+        }
         memset((void *)node, 0, sizeof(HDNode));
-    } 
+    }
 
 process_ethereum_xfer_exit:
     return(ret_val);
@@ -112,7 +112,7 @@ static int process_ethereum_msg(EthereumSignTx *msg, bool *confirm_ptr)
                 ret_result = TXOUT_OK;
                 break;
         }
-    } 
+    }
     return(ret_result);
 }
 
@@ -133,7 +133,7 @@ void fsm_msgEthereumSignTx(EthereumSignTx *msg)
     bool needs_confirm = true;
     int msg_result = process_ethereum_msg(msg, &needs_confirm);
 
-    if(msg_result < TXOUT_OK) 
+    if(msg_result < TXOUT_OK)
     {
         send_fsm_co_error_message(msg_result);
         go_home();
