bf75652a780a4766880f554417a3eb3bd73cf866
keepkeyjon <jon@keepkey.com>
2018-08-30 18:10:19 -0600

    Add ZEC, and Testnet ZEC

--- include/keepkey/firmware/coins.h
+++ include/keepkey/firmware/coins.h
@@ -26,7 +26,7 @@
 #define ETHEREUM        "Ethereum"
 #define ETHEREUM_CLS    "Ethereum Classic"
 
-#define COINS_COUNT         45
+#define COINS_COUNT         47
 #define NODE_STRING_LENGTH  50
 
 #define COIN_FRACTION 100000000
