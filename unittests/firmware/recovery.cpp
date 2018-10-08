ed1378251e3a006996f91c3df8597fa39bb8dfa2
keepkeyjon <jon@keepkey.com>
2018-10-08 14:59:32 -0600

    keepkey/trezor-crypto b768a75f added back the wordlist padding

--- unittests/firmware/recovery.cpp
+++ unittests/firmware/recovery.cpp
@@ -18,12 +18,6 @@ TEST(Recovery, ExactStrMatch) {
 
 bool attempt_auto_complete(char *partial_word);
 
-#if !defined(__has_feature)
-#  define __has_feature(X) 0
-#endif
-
-#if !__has_feature(address_sanitizer) && !__SANITIZE_ADDRESS__
-// FIXME: the bip32 wordlist needs padding added back.
 TEST(Recovery, AutoComplete) {
     char partial_word[] = "all\0\0\0\0\0";
     ASSERT_TRUE(attempt_auto_complete(partial_word));
@@ -37,4 +31,3 @@ TEST(Recovery, AutoComplete) {
     ASSERT_FALSE(attempt_auto_complete(partial_word));
     ASSERT_TRUE(memcmp(partial_word, "allways\0", sizeof(partial_word)) == 0);
 }
-#endif
