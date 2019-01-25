keepkey/keepkey-firmware

commit 6746fd0c993db7e4da32b4a924c9821d1e0b5c38

Implement SLIP48

include/keepkey/firmware/coins.h
@@ -95,4 +95,15 @@ bool bip32_node_to_string(char *node_str, size_t len, const CoinType *coin,
 /// \returns true iff the coin_name is for an eth-like coin.
 bool isEthereumLike(const char *coin_name);
 
+typedef enum _SLIP48Role {
+    SLIP48_owner   = 0x0,
+    SLIP48_active  = 0x1,
+    SLIP48_memo    = 0x3,
+    SLIP48_posting = 0x4,
+    SLIP48_UNKNOWN = 0xffffffff,
+} SLIP48Role;
+
+bool coin_isSLIP48(const CoinType *coin, const uint32_t *address_n,
+                   size_t address_n_count, SLIP48Role role);
+
 #endif

lib/firmware/coins.c
@@ -382,16 +382,21 @@ bool bip32_node_to_string(char *node_str, size_t len, const CoinType *coin,
     if (address_n_count != 3 && address_n_count != 5)
         return false;
 
-    // Don't display this way for change addresses, discouraging their use in GetAddress.
     if (!whole_account) {
         if (address_n_count != 5)
             return false;
 
-        if (address_n[3] != 0)
+        // Don't display this way for change addresses,
+        // discouraging their use in GetAddress.
+        if (address_n[3] != 0 &&
+            !coin_isSLIP48(coin, address_n, address_n_count, SLIP48_owner) &&
+            !coin_isSLIP48(coin, address_n, address_n_count, SLIP48_active))
             return false;
     }
 
-    if (path_mismatched(coin, address_n, address_n_count, whole_account))
+    if (path_mismatched(coin, address_n, address_n_count, whole_account) &&
+        !coin_isSLIP48(coin, address_n, address_n_count, SLIP48_owner) &&
+        !coin_isSLIP48(coin, address_n, address_n_count, SLIP48_active))
         return false;
 
     const char *prefix = account_prefix(coin, address_n, address_n_count, whole_account);
@@ -405,6 +410,12 @@ bool bip32_node_to_string(char *node_str, size_t len, const CoinType *coin,
     if (whole_account || isEthereumLike(coin_name)) {
         snprintf(node_str, len, "%s%s Account #%" PRIu32, prefix, coin_name,
                  address_n[2] & 0x7ffffff);
+    } else if (coin_isSLIP48(coin, address_n, address_n_count, SLIP48_owner)) {
+        snprintf(node_str, len, "%s%s Account #%" PRIu32 " @owner", prefix, coin_name,
+                 address_n[3] & 0x7ffffff);
+    } else if (coin_isSLIP48(coin, address_n, address_n_count, SLIP48_active)) {
+        snprintf(node_str, len, "%s%s Account #%" PRIu32 " @active", prefix, coin_name,
+                 address_n[3] & 0x7ffffff);
     } else {
         snprintf(node_str, len, "%s%s Account #%" PRIu32 "\nAddress #%" PRIu32, prefix, coin_name,
                  address_n[2] & 0x7ffffff, address_n[4]);
@@ -422,4 +433,62 @@ bool isEthereumLike(const char *coin_name)
         return true;
 
     return false;
+}
+
+static bool role_matches(uint32_t address_n_role, SLIP48Role role) {
+    switch (role) {
+    case SLIP48_owner:
+        return address_n_role == (0x80000000 | 0x0);
+
+    case SLIP48_active:
+        return address_n_role == (0x80000000 | 0x1);
+
+    case SLIP48_memo:
+        return address_n_role == (0x80000000 | 0x3);
+
+    case SLIP48_posting:
+        return address_n_role == (0x80000000 | 0x4);
+
+    case SLIP48_UNKNOWN:
+        return false;
+    }
+
+#ifdef DEBUG_ON
+    __builtin_unreachable();
+#else
+    return false;
+#endif
+}
+
+bool coin_isSLIP48(const CoinType *coin, const uint32_t *address_n,
+                   size_t address_n_count, SLIP48Role role) {
+
+    // Assume EOS for now. We'll expand this as we add more graphene coins.
+    if (strncmp(coin->coin_name, "EOS", sizeof(coin->coin_name)))
+        return false;
+
+    if (address_n_count < 5)
+        return false;
+
+    // Purpose
+    if (address_n[address_n_count - 5] != (0x80000000 | 48))
+        return false;
+
+    // Network
+    if (address_n[address_n_count - 4] != (0x80000000 | /*EOS=*/4))
+        return false;
+
+    // Role
+    if (!role_matches(address_n[address_n_count - 3], role))
+        return false;
+
+    // Account Index
+    if (!(address_n[address_n_count - 2] & 0x80000000))
+        return false;
+
+    // Key Index
+    if (!(address_n[address_n_count - 1] & 0x80000000))
+        return false;
+
+    return true;
 }

lib/firmware/eos-contracts/eosio.system.c
@@ -499,10 +499,14 @@ static bool authorizationIsDeviceControlled(const EosAuthorization *auth) {
     return true;
 }
 
-bool eos_compileAuthorization(const char *title, const EosAuthorization *auth) {
+bool eos_compileAuthorization(const char *title, const EosAuthorization *auth,
+                              SLIP48Role role) {
     CHECK_PARAM_RET(auth->has_threshold, "Required field missing", false);
 
-    if (authorizationIsDeviceControlled(auth)) {
+
+    if (authorizationIsDeviceControlled(auth) &&
+        coin_isSLIP48(coinByName("EOS"), auth->keys[0].address_n,
+                      auth->keys[0].address_n_count, role)) {
         const EosAuthorizationKey *auth_key = &auth->keys[0];
 
         char node_str[NODE_STRING_LENGTH];
@@ -522,7 +526,7 @@ bool eos_compileAuthorization(const char *title, const EosAuthorization *auth) {
         }
 
         if (!confirm(ButtonRequestType_ButtonRequest_ConfirmEosAction,
-                     title, "Do you want to assign signing auth for %s to %s?",
+                     title, "Do you want to assign signing auth for\n%s to\n%s?",
                      title, node_str)) {
             fsm_sendFailure(FailureType_Failure_ActionCancelled, "Action Cancelled");
             eos_signingAbort();
@@ -560,10 +564,6 @@ bool eos_compileAuthorization(const char *title, const EosAuthorization *auth) {
         } else {
             const CoinType *coin;
             if ((coin = coinByName("EOS")) &&
-                !bip32_node_to_string(pubkey, sizeof(pubkey), coin,
-                                      auth_key->address_n,
-                                      auth_key->address_n_count,
-                                      /*whole_account=*/false) &&
                 !bip32_path_to_string(pubkey, sizeof(pubkey),
                                       auth_key->address_n, auth_key->address_n_count)) {
                 memset(pubkey, 0, sizeof(pubkey));
@@ -627,6 +627,14 @@ bool eos_compileAuthorization(const char *title, const EosAuthorization *auth) {
     return true;
 }
 
+static SLIP48Role roleFromPermission(uint64_t permission) {
+    switch (permission) {
+    case EOS_Owner: return SLIP48_owner;
+    case EOS_Active: return SLIP48_active;
+    default: return SLIP48_UNKNOWN;
+    }
+}
+
 bool eos_compileActionUpdateAuth(const EosActionCommon *common,
                                  const EosActionUpdateAuth *action) {
     CHECK_COMMON(EOS_UpdateAuth);
@@ -672,7 +680,9 @@ bool eos_compileActionUpdateAuth(const EosActionCommon *common,
     hasher_Update(&hasher_preimage, (const uint8_t*)&action->permission, 8);
     hasher_Update(&hasher_preimage, (const uint8_t*)&action->parent, 8);
 
-    if (!eos_compileAuthorization(title, &action->auth))
+    snprintf(title, sizeof(title), "%s@%s", account, permission);
+    if (!eos_compileAuthorization(title, &action->auth,
+                                  roleFromPermission(action->permission)))
         return false;
 
     return true;
@@ -853,11 +863,11 @@ bool eos_compileActionNewAccount(const EosActionCommon *common,
 
     char title[SMALL_STR_BUF];
     snprintf(title, sizeof(title), "%s@owner", name);
-    if (!eos_compileAuthorization(title, &action->owner))
+    if (!eos_compileAuthorization(title, &action->owner, SLIP48_owner))
         return false;
 
     snprintf(title, sizeof(title), "%s@active", name);
-    if (!eos_compileAuthorization(title, &action->active))
+    if (!eos_compileAuthorization(title, &action->active, SLIP48_active))
         return false;
 
     return true;

unittests/firmware/coins.cpp
@@ -82,3 +82,50 @@ TEST(Coins, TableSanity) {
             << "Contract address mismatch for " << coin.coin_shortcut;
     }
 }
+
+TEST(Coins, SLIP48) {
+    struct {
+        const char *coin_name;
+        uint32_t address_n[10];
+        size_t address_n_count;
+        SLIP48Role role;
+        bool isSLIP48;
+        std::string text;
+    } vector[] = {
+        {
+          "EOS",
+          { 0x80000000|48, 0x80000000|4, 0x80000000|0, 0x80000000|0, 0x80000000|0 },
+          5, SLIP48_owner, true, "EOS Account #0 @owner"
+        },
+        {
+          "EOS",
+          { 0x80000000|48, 0x80000000|4, 0x80000000|1, 0x80000000|3, 0x80000000|0 },
+          5, SLIP48_active, true, "EOS Account #3 @active"
+        },
+        {
+          "EOS",
+          { 0x80000000|48, 0x80000000|4, 0x80000000|1, 0x80000000|7, 0x80000000|0 },
+          5, SLIP48_active, true, "EOS Account #7 @active"
+        },
+        {
+          "EOS",
+          { 0x80000000|48, 0x80000000|4, 0x80000000|1, 0x80000000|0, 0x80000000|0 },
+          4, SLIP48_UNKNOWN, false, ""
+        },
+    };
+
+    for (const auto &vec : vector) {
+        EXPECT_EQ(coin_isSLIP48(coinByName(vec.coin_name), vec.address_n,
+                                vec.address_n_count, vec.role), vec.isSLIP48);
+
+        if (vec.isSLIP48) {
+            char node_str[NODE_STRING_LENGTH];
+            ASSERT_TRUE(bip32_node_to_string(node_str, sizeof(node_str),
+                                             coinByName(vec.coin_name),
+                                             vec.address_n,
+                                             vec.address_n_count,
+                                             /*whole_account=*/false));
+            EXPECT_EQ(vec.text, node_str);
+        }
+    }
+}

include/keepkey/firmware/eos-contracts/eosio.system.h
@@ -20,6 +20,8 @@
 #ifndef KEEPKEY_FIRMWARE_EOS_CONTRACTS_EOSIOSYSTEM_H
 #define KEEPKEY_FIRMWARE_EOS_CONTRACTS_EOSIOSYSTEM_H
 
+#include "keepkey/firmware/coins.h"
+
 #include <stdbool.h>
 
 typedef struct _EosActionBuyRam EosActionBuyRam;
@@ -66,7 +68,8 @@ bool eos_compileActionVoteProducer(const EosActionCommon *common,
                                    const EosActionVoteProducer *action);
 
 /// \returns true iff successful.
-bool eos_compileAuthorization(const char *title, const EosAuthorization *auth);
+bool eos_compileAuthorization(const char *title, const EosAuthorization *auth,
+                              SLIP48Role role);
 
 /// \returns true iff successful.
 bool eos_compileActionUpdateAuth(const EosActionCommon *common,

commit 8b4e608a14b06b93608a4e60bf82b2e29f790c26

bip32_node_to_string needs to work for change... sometimes
notably in ShapeShift exchange flow, and internal transfers.

fixes: TestMsgSigntxSegwit.test_attack_change_input_address, which was
broken by 6746fd0c9 (I think?)

include/keepkey/firmware/coins.h
@@ -86,11 +86,12 @@ bool bip32_path_to_string(char *str, size_t len, const uint32_t *address_n, size
  * \param[in]   address_n        node path
  * \param[in]   address_n_count  size of address_n array
  * \param[in]   whole_account    true iff address_n refers to an entire account (not just an address)
+ * \param[in]   allow_change     whether to allow display for change addressses
  * \returns true iff the path matches a known bip44/bip49/bip84/etc account
  */
 bool bip32_node_to_string(char *node_str, size_t len, const CoinType *coin,
                           const uint32_t *address_n, size_t address_n_count,
-                          bool whole_account);
+                          bool whole_account, bool allow_change);
 
 /// \returns true iff the coin_name is for an eth-like coin.
 bool isEthereumLike(const char *coin_name);

lib/firmware/coins.c
@@ -353,7 +353,7 @@ void coin_amnt_to_str(const CoinType *coin, uint64_t amnt, char *buf, int len)
 
 bool bip32_node_to_string(char *node_str, size_t len, const CoinType *coin,
                           const uint32_t *address_n, size_t address_n_count,
-                          bool whole_account)
+                          bool whole_account, bool allow_change)
 {
     if (address_n_count != 3 && address_n_count != 5)
         return false;
@@ -372,7 +372,7 @@ bool bip32_node_to_string(char *node_str, size_t len, const CoinType *coin,
         // Don't display this way for change addresses,
         // discouraging their use in GetAddress.
         if (address_n[3] != 0 && !isSLIP48)
-            return false;
+            return allow_change;
     }
 
     if (path_mismatched(coin, address_n, address_n_count, whole_account) &&

lib/firmware/eos-contracts/eosio.system.c
@@ -515,7 +515,8 @@ bool eos_compileAuthorization(const char *title, const EosAuthorization *auth,
             !bip32_node_to_string(node_str, sizeof(node_str), coin,
                                   auth_key->address_n,
                                   auth_key->address_n_count,
-                                  /*whole_account=*/false) &&
+                                  /*whole_account=*/false,
+                                  /*allow_change=*/false) &&
             !bip32_path_to_string(node_str, sizeof(node_str),
                                   auth_key->address_n, auth_key->address_n_count)) {
             memset(node_str, 0, sizeof(node_str));

lib/firmware/exchange.c
@@ -533,7 +533,8 @@ bool process_exchange_contract(const CoinType *coin, void *vtx_out, const HDNode
     if (!bip32_node_to_string(node_str, sizeof(node_str), withdrawal_coin,
                              tx_exchange->withdrawal_address_n,
                              tx_exchange->withdrawal_address_n_count,
-                             /*whole_account=*/false)) {
+                             /*whole_account=*/false,
+                             /*allow_change=*/true)) {
         set_exchange_error(ERROR_EXCHANGE_WITHDRAWAL_ADDRESS);
         return false;
     }

lib/firmware/fsm_msg_coin.h
@@ -55,7 +55,8 @@ void fsm_msgGetPublicKey(GetPublicKey *msg)
 		if (!bip32_node_to_string(node_str, sizeof(node_str), coin,
 		                          msg->address_n,
 		                          msg->address_n_count,
-		                          /*whole_account=*/true) &&
+		                          /*whole_account=*/true,
+		                          /*allow_change=*/false) &&
 		    !bip32_path_to_string(node_str, sizeof(node_str),
 		                          msg->address_n, msg->address_n_count)) {
 			memset(node_str, 0, sizeof(node_str));
@@ -211,7 +212,8 @@ void fsm_msgGetAddress(GetAddress *msg)
 			         msg->multisig.m, (uint32_t)msg->multisig.pubkeys_count);
 		} else {
 			if (!bip32_node_to_string(node_str, sizeof(node_str), coin, msg->address_n,
-			                          msg->address_n_count, /*whole_account=*/false) &&
+			                          msg->address_n_count, /*whole_account=*/false,
+			                          /*allow_change=*/false) &&
 			    !bip32_path_to_string(node_str, sizeof(node_str),
 			                          msg->address_n, msg->address_n_count)) {
 				memset(node_str, 0, sizeof(node_str));

lib/firmware/fsm_msg_eos.h
@@ -53,7 +53,8 @@ void fsm_msgEosGetPublicKey(const EosGetPublicKey *msg) {
         if (!bip32_node_to_string(node_str, sizeof(node_str), coin,
                                   msg->address_n,
                                   msg->address_n_count,
-                                  /*whole_account=*/false) &&
+                                  /*whole_account=*/false,
+                                  /*allow_change=*/false) &&
             !bip32_path_to_string(node_str, sizeof(node_str),
                                   msg->address_n, msg->address_n_count)) {
             memset(node_str, 0, sizeof(node_str));

lib/firmware/fsm_msg_ethereum.h
@@ -9,7 +9,8 @@ static int process_ethereum_xfer(const CoinType *coin, EthereumSignTx *msg)
 
     char node_str[NODE_STRING_LENGTH];
     if (!bip32_node_to_string(node_str, sizeof(node_str), coin, msg->to_address_n,
-                              msg->to_address_n_count, /*whole_account=*/false))
+                              msg->to_address_n_count, /*whole_account=*/false,
+                              /*allow_change=*/false))
         return TXOUT_COMPILE_ERROR;
 
     bool *has_to;
@@ -170,7 +171,8 @@ void fsm_msgEthereumGetAddress(EthereumGetAddress *msg)
 		      bip32_node_to_string(node_str, sizeof(node_str), coin,
 		                           msg->address_n,
 		                           msg->address_n_count,
-		                           /*whole_account=*/false)) &&
+		                           /*whole_account=*/false,
+		                           /*allow_change=*/false)) &&
 		    !bip32_path_to_string(node_str, sizeof(node_str),
 		                          msg->address_n, msg->address_n_count)) {
 			memset(node_str, 0, sizeof(node_str));

lib/firmware/transaction.c
@@ -367,7 +367,8 @@ int compile_output(const CoinType *coin, const HDNode *root, TxOutputType *in, T
 				coin_amnt_to_str(coin, in->amount, amount_str, sizeof(amount_str));
 				memset(node_str, 0, sizeof(node_str));
 				if (!bip32_node_to_string(node_str, sizeof(node_str), coin, in->address_n,
-				                          in->address_n_count, /*whole_account=*/false))
+				                          in->address_n_count, /*whole_account=*/false,
+				                          /*allow_change=*/true))
 					break;
 				if (!confirm_transfer_output(ButtonRequestType_ButtonRequest_ConfirmTransferToAccount, amount_str, node_str))
 					return TXOUT_CANCEL;

unittests/firmware/coins.cpp
@@ -125,7 +125,8 @@ TEST(Coins, SLIP48) {
                                              coinByName(vec.coin_name),
                                              vec.address_n,
                                              vec.address_n_count,
-                                             /*whole_account=*/false))
+                                             /*whole_account=*/false,
+                                             /*allow_change=*/false))
                 << vec.text;
             EXPECT_EQ(vec.text, node_str);
         }
@@ -137,34 +138,46 @@ TEST(Coins, BIP32AccountName) {
         const char *coin_name;
         uint32_t address_n[10];
         size_t address_n_count;
+        bool allow_change;
         bool expected;
         std::string text;
     } vector[] = {
         {
           "Bitcoin",
           { 0x80000000|44, 0x80000000|0, 0x80000000|0, 0, 0 },
-          5, true, "Bitcoin Account #0\nAddress #0"
+          5, false, true, "Bitcoin Account #0\nAddress #0"
         },
         {
           "Bitcoin",
           { 0x80000000|44, 0x80000000|0, 0x80000000|0, 0, 1 },
-          5, true, "Bitcoin Account #0\nAddress #1"
+          5, false, true, "Bitcoin Account #0\nAddress #1"
         },
         {
           "Bitcoin",
           { 0x80000000|44, 0x80000000|0, 0x80000000|1, 0, 0 },
-          5, true, "Bitcoin Account #1\nAddress #0"
+          5, false, true, "Bitcoin Account #1\nAddress #0"
         },
         {
           "Bitcoin",
           { 0x80000000|44, 0x80000000|0, 0x80000000|1, 0, 1 },
-          5, true, "Bitcoin Account #1\nAddress #1"
+          5, false, true, "Bitcoin Account #1\nAddress #1"
         },
         {
           "Bitcoin",
           { 0x80000000|44, 0x80000000|0, 0x80000000|1, 1, 1 },
-          5, false, ""
+          5, false, false, ""
         },
+        {
+          "Bitcoin",
+          { 0x80000000|44, 0x80000000|0, 0x80000000|1, 1, 1 },
+          5, true, true, "Bitcoin Account #1\nAddress #1"
+        },
+        {
+          "Bitcoin",
+          { 0x80000000|44, 0x80000000|0, 0x80000000|1, 1, 1 },
+          5, false, false, ""
+        },
+
     };
 
     for (const auto &vec : vector) {
@@ -173,7 +186,8 @@ TEST(Coins, BIP32AccountName) {
                                        coinByName(vec.coin_name),
                                        vec.address_n,
                                        vec.address_n_count,
-                                       /*whole_account=*/false),
+                                       /*whole_account=*/false,
+                                       vec.allow_change),
                   vec.expected)
             << vec.text;
         if (vec.expected) {