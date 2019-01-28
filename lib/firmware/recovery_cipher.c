ac80842b24d9e3a760be6432b5bb047b63c2e313
keepkeyjon <jon@keepkey.com>
2019-01-28 14:53:02 -0700

    Early return. NFC

--- lib/firmware/recovery_cipher.c
+++ lib/firmware/recovery_cipher.c
@@ -259,52 +259,44 @@ void next_character(void)
     get_current_word(current_word);
 
     /* Words should never be longer than 4 characters */
-    if (strlen(current_word) > 4)
-    {
+    if (strlen(current_word) > 4) {
         memzero(current_word, sizeof(current_word));
         awaiting_character = false;
         layoutHome();
 
         storage_reset();
         fsm_sendFailure(FailureType_Failure_SyntaxError, "Words were not entered correctly.");
+        return;
     }
-    else
-    {
-        CharacterRequest resp;
-        memset(&resp, 0, sizeof(CharacterRequest));
 
-        resp.word_pos = get_current_word_pos();
-        resp.character_pos = strlen(current_word);
+    CharacterRequest resp;
+    memset(&resp, 0, sizeof(CharacterRequest));
 
-        msg_write(MessageType_MessageType_CharacterRequest, &resp);
+    resp.word_pos = get_current_word_pos();
+    resp.character_pos = strlen(current_word);
 
-        /* Attempt to auto complete if we have at least 3 characters */
-        bool auto_completed = false;
-        if (strlen(current_word) >= 3)
-        {
-            auto_completed = attempt_auto_complete(current_word);
-        }
+    msg_write(MessageType_MessageType_CharacterRequest, &resp);
+
+    /* Attempt to auto complete if we have at least 3 characters */
+    bool auto_completed = false;
+    if (strlen(current_word) >= 3) {
+        auto_completed = attempt_auto_complete(current_word);
+    }
 
 #if DEBUG_LINK
-
-        if(auto_completed)
-        {
-            strlcpy(auto_completed_word, current_word, CURRENT_WORD_BUF);
-        }
-        else
-        {
-            auto_completed_word[0] = '\0';
-        }
-
+    if (auto_completed) {
+        strlcpy(auto_completed_word, current_word, CURRENT_WORD_BUF);
+    } else {
+        auto_completed_word[0] = '\0';
+    }
 #endif
 
-        /* Format current word and display it along with cipher */
-        format_current_word(current_word, auto_completed);
+    /* Format current word and display it along with cipher */
+    format_current_word(current_word, auto_completed);
 
-        /* Show cipher and partial word */
-        layout_cipher(current_word, cipher);
-        memzero(current_word, sizeof(current_word));
-    }
+    /* Show cipher and partial word */
+    layout_cipher(current_word, cipher);
+    memzero(current_word, sizeof(current_word));
 }
 
 /*
