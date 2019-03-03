075fae288f58225636041b890c535a6c4c3a6e61
keepkeyjon <jon@keepkey.com>
2019-03-03 09:43:12 -0700

    Confirm ERC721 'transferFrom'

---
+++ include/keepkey/firmware/ethereum_contracts.h
@@ -0,0 +1,36 @@
+#/*
+ * This file is part of the KeepKey project.
+ *
+ * Copyright (C) 2019 ShapeShift
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef KEEPKEY_FIRMWARE_ETHEREUMCONTRACTS_H
+#define KEEPKEY_FIRMWARE_ETHEREUMCONTRACTS_H
+
+#include "trezor/crypto/bip32.h"
+
+typedef struct _EthereumSignTx EthereumSignTx;
+
+/// \returns true iff there is custom support for this ETH signing request
+bool ethereum_contractHandled(uint32_t data_total, const EthereumSignTx *msg,
+                              const HDNode *node);
+
+/// \pre requires that `ethereum_contractHandled(msg)`
+/// \return true iff the user has confirmed the custom ETH signing request
+bool ethereum_contractConfirmed(uint32_t data_total, const EthereumSignTx *msg,
+                                const HDNode *node);
+
+#endif
