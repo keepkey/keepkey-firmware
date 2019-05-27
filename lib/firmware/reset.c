05052718f9fb5f4cf130bfedf69351b773b2f330
keepkeyjon <jon@keepkey.com>
2019-05-27 11:53:13 -0600

    reset: show some on-device instructions

--- lib/firmware/reset.c
+++ lib/firmware/reset.c
@@ -150,6 +150,15 @@ void reset_entropy(const uint8_t *ext_entropy, uint32_t len)
         storage_commit();
         fsm_sendSuccess(_("Device reset"));
         goto exit;
+    } else {
+        if (!confirm(ButtonRequestType_ButtonRequest_Other, _("Recovery Sentence Bakcup"),
+                     "Your recovery sentence will only be shown ONCE. "
+                     "Please write it down carefully,\n"
+                     "and DO NOT share it with anyone. ")) {
+            fsm_sendFailure(FailureType_Failure_ActionCancelled, _("Reset cancelled"));
+            layoutHome();
+            return;
+        }
     }
 
     /*
@@ -213,7 +222,7 @@ void reset_entropy(const uint8_t *ext_entropy, uint32_t len)
     /* Have user confirm mnemonic is sets of 12 words */
     for(uint32_t current_page = 0; current_page < page_count; current_page++)
     {
-        char title[MEDIUM_STR_BUF] = _("Recovery Sentence");
+        char title[MEDIUM_STR_BUF] = _("Recovery Sentence Backup");
 
         /* make current screen mnemonic available via debuglink */
         strlcpy(current_words, mnemonic_by_screen[current_page], MNEMONIC_BY_SCREEN_BUF);
@@ -221,7 +230,7 @@ void reset_entropy(const uint8_t *ext_entropy, uint32_t len)
         if(page_count > 1)
         {
             /* snprintf: 20 + 10 (%d) + 1 (NULL) = 31 */
-            snprintf(title, MEDIUM_STR_BUF, _("Recovery Sentence %" PRIu32 "/%" PRIu32 ""), current_page + 1, page_count);
+            snprintf(title, MEDIUM_STR_BUF, _("Recovery Sentence Backup %" PRIu32 "/%" PRIu32 ""), current_page + 1, page_count);
         }
 
         if(!confirm(ButtonRequestType_ButtonRequest_ConfirmWord, title, "%s",
