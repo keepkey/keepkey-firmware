36c1d0d35183ba2daf6d3b5170f8a30bc90d12d6
tester <Bithighlander@gmail.com>
2019-09-04 16:55:35 -0600

    pre-signtx revamp

--- include/keepkey/firmware/cosmos.h
+++ include/keepkey/firmware/cosmos.h
@@ -15,8 +15,8 @@
 
 typedef struct _CosmosSignTx CosmosSignTx;
 typedef struct _CosmosTxAck CosmosTxAck;
-typedef struct _CosmosSignMessage CosmosSignMessage;
-typedef struct _CosmosVerifyMessage CosmosVerifyMessage;
+//typedef struct _CosmosSignMessage CosmosSignMessage;
+//typedef struct _CosmosVerifyMessage CosmosVerifyMessage;
 typedef struct _CosmosMessageSignature CosmosMessageSignature;
 typedef struct _TokenType TokenType;
 typedef struct _CoinType CoinType;
@@ -24,8 +24,8 @@ typedef struct _CoinType CoinType;
 void cosmos_signing_init(CosmosSignTx *msg, const HDNode *node, bool needs_confirm);
 void cosmos_signing_abort(void);
 void cosmos_signing_txack(CosmosTxAck *msg);
-void format_cosmos_address(const uint8_t *to, char *destination_str,
-                             uint32_t destination_str_len);
+//void format_cosmos_address(const uint8_t *to, char *destination_str,
+//                             uint32_t destination_str_len);
 
 /**
  * \brief Get the number of decimals associated with an erc20 token
@@ -34,7 +34,7 @@ void format_cosmos_address(const uint8_t *to, char *destination_str,
  */
 uint32_t cosmos_get_decimal(const char *token_shortcut);
 
-void cosmos_message_sign(const CosmosSignMessage *msg, const HDNode *node, CosmosMessageSignature *resp);
-int cosmos_message_verify(const CosmosVerifyMessage *msg);
+//void cosmos_message_sign(const CosmosSignMessage *msg, const HDNode *node, CosmosMessageSignature *resp);
+//int cosmos_message_verify(const CosmosVerifyMessage *msg);
 
 void cosmosFormatAmount(const bignum256 *amnt, const TokenType *token, uint32_t chain_id, char *buf, int buflen);
