081903bfdcf74aa31d4b20c0557fe8dc978f06d3
keepkeyjon <jon@keepkey.com>
2020-01-13 13:43:51 -0700

    firmware: add missing abort

--- lib/firmware/fsm_msg_common.h
+++ lib/firmware/fsm_msg_common.h
@@ -4,6 +4,7 @@ void fsm_msgInitialize(Initialize *msg)
     recovery_cipher_abort();
     signing_abort();
     ethereum_signing_abort();
+    cosmos_signAbort();
     eos_signingAbort();
     session_clear(false); // do not clear PIN
     layoutHome();
@@ -383,6 +384,7 @@ void fsm_msgCancel(Cancel *msg)
     recovery_cipher_abort();
     signing_abort();
     ethereum_signing_abort();
+    cosmos_signAbort();
     eos_signingAbort();
     fsm_sendFailure(FailureType_Failure_ActionCancelled, "Aborted");
 }
