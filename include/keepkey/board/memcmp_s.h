3f67e92cb1a29584d0ff18dd2b27770074cf56b0
keepkeyjon <jon@keepkey.com>
2020-02-03 12:01:59 -0700

    board: use 1-of-N decoy compares
    ... to further obfuscate the power profile and timing of memcmp_s.

--- include/keepkey/board/memcmp_s.h
+++ include/keepkey/board/memcmp_s.h
@@ -1,13 +1,12 @@
 #ifndef KEEPKEY_BOARD_MEMCMPS_H
 #define KEEPKEY_BOARD_MEMCMPS_H
 
-#ifndef EMULATOR
+#include <stddef.h>
+
 /// Compare two memory regions for equality in constant time.
+/// \param lhs must be an array of at least `len` bytes
+/// \param rhs must be an array of at least `len` bytes
+/// \param len must be in [32, 255), otherwise behavior is undefined.
 int memcmp_s(const void *lhs, const void *rhs, size_t len);
-#else
-#  include <string.h>
-#  warning "memcmp_s is not guaranteed to be constant-time on this architecture"
-#  define memcmp_s(lhs, rhs, len) memcmp((lhs), (rhs), (len))
-#endif
 
 #endif
