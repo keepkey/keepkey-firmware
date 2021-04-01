7d733b2cb43e100a619a87c6225e72143ab93407
mcchadwick <nick.mor@shapeshift.io>
2021-04-01 10:51:35 -0600

    Add verbose flag to test suite run script

--- scripts/emulator/python-keepkey-tests.sh
+++ scripts/emulator/python-keepkey-tests.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /kkemu/test-reports/python-keepkey
 cd deps/python-keepkey/tests
-KK_TRANSPORT_MAIN=kkemu:21324 KK_TRANSPORT_DEBUG=kkemu:21325 pytest --junitxml=/kkemu/test-reports/python-keepkey/junit.xml
+KK_TRANSPORT_MAIN=kkemu:21324 KK_TRANSPORT_DEBUG=kkemu:21325 pytest -v --junitxml=/kkemu/test-reports/python-keepkey/junit.xml
 echo "$?" > /kkemu/test-reports/python-keepkey/status
