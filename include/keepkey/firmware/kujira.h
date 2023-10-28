0860841bc173879dfc6bcf7ba7a67e9ba7670a84
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2023-10-28 17:08:24 +0200

    Create kujira.h

---
+++ include/keepkey/firmware/kujira.h
@@ -0,0 +1,12 @@
+#ifndef KEEPKEY_FIRMWARE_KUJIRA_H
+#define KEEPKEY_FIRMWARE_KUJIRA_H
+
+#include "messages.pb.h"
+#include "trezor/crypto/bip32.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct _KujiraSignTx KujiraSignTx;
+
+#endif
