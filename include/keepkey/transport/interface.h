72f8ef50524138ec2650b16bfa82ab56a7d7d2c0
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2023-10-28 17:11:29 +0200

    Update interface.h

--- include/keepkey/transport/interface.h
+++ include/keepkey/transport/interface.h
@@ -35,6 +35,7 @@
 #include "messages-tendermint.pb.h"
 #include "messages-thorchain.pb.h"
 #include "messages-mayachain.pb.h"
+#include "messages-kujira.pb.h"
 
 #include "types.pb.h"
 #include "trezor_transport.h"
