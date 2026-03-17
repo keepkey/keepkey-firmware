b4dd38b5b79a5ddaf198f7855b76fc08e4a08988
highlander <highlander@keepkey.com>
2026-03-16 20:05:30 -0600

    fix: add Solana/Tron/TON proto includes to interface.h with BTC_ONLY guard

--- include/keepkey/transport/interface.h
+++ include/keepkey/transport/interface.h
@@ -26,6 +26,7 @@
 #include "messages-nano.pb.h"
 #undef delete
 
+#ifndef BITCOIN_ONLY
 #include "messages-ethereum.pb.h"
 #include "messages-binance.pb.h"
 #include "messages-cosmos.pb.h"
@@ -35,6 +36,10 @@
 #include "messages-tendermint.pb.h"
 #include "messages-thorchain.pb.h"
 #include "messages-mayachain.pb.h"
+#include "messages-solana.pb.h"
+#include "messages-tron.pb.h"
+#include "messages-ton.pb.h"
+#endif // BITCOIN_ONLY
 
 #include "types.pb.h"
 #include "trezor_transport.h"
